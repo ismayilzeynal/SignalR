7185ce35a003824b64f158fb1533090623aecf4c
ismayilzeynal <ismayilbz@code.edu.az>
2023-03-25 23:47:16 +0400

    feature: send and recieve message by signalR lib (client<->server)

---
+++ FrontToBack/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Message()
+        {
+            return View();
+        }
+    }
+}
--- FrontToBack/Hubs/ChatHub.cs
+++ FrontToBack/Hubs/ChatHub.cs
@@ -4,5 +4,10 @@ namespace FrontToBack.Hubs
 {
     public class ChatHub:Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
     }
 }
+
