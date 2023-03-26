5026979bf0aa721a05a6600cbb03a49031f0ecf7
ismayilzeynal <ismayilbz@code.edu.az>
2023-03-27 00:08:02 +0400

    feature: user to user message (demo)

--- FrontToBack/Controllers/ChatController.cs
+++ FrontToBack/Controllers/ChatController.cs
@@ -1,17 +1,23 @@
 ﻿using FrontToBack.DAL;
+using FrontToBack.Hubs;
 using FrontToBack.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.SignalR;
 
 namespace FrontToBack.Controllers
 {
+    
     public class ChatController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly IHubContext<ChatHub> _hubContext;
 
-        public ChatController(UserManager<AppUser> userManager)
+        public ChatController(UserManager<AppUser> userManager, IHubContext<ChatHub> hubContext)
         {
             _userManager = userManager;
+            _hubContext = hubContext;
         }
 
         public IActionResult Message()
@@ -21,6 +27,14 @@ namespace FrontToBack.Controllers
 
 
             return View();
+        }
+
+        public async Task<IActionResult> ShowAlert(string UserId)
+        {
+            AppUser user = await _userManager.FindByIdAsync(UserId);
+            _hubContext.Clients.Client(user.ConnectionId).SendAsync("ShowAlert", "hello");
+            // _hubContext.Clients.Clients() // toplu, qrup
+            return RedirectToAction("message");
         }
     }
 }
