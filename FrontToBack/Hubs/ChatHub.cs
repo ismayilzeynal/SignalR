e240269ed1c4098195ee9d9fdf22cf863e2757f4
ismayilzeynal <ismayilbz@code.edu.az>
2023-03-27 03:33:10 +0400

    feat: send message with message sender username

--- FrontToBack/Hubs/ChatHub.cs
+++ FrontToBack/Hubs/ChatHub.cs
@@ -7,24 +7,27 @@ namespace FrontToBack.Hubs
     public class ChatHub:Hub
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public ChatHub(UserManager<AppUser> userManager)
+        public ChatHub(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        
+
         public async Task SendMessage(string user, string message) // old version
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task SendMessage2(string[] users, string user, string message)
+        public async Task SendMessage2(string[] users, string message)
         {
+
             foreach (var item in users)
             {
                 AppUser User = await _userManager.FindByIdAsync(item);
-                await Clients.Client(User.ConnectionId).SendAsync("ReceiveMessage2", user, message);
+                await Clients.Client(User.ConnectionId).SendAsync("ReceiveMessage2", Context.User.Identity.Name, message); //user
             }
         }
 
